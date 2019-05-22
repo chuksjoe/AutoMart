a48e4c3ac0f0163b2f3ac5a8f33c9b3bb132eed2
Chukwunonso Orjiakor <chuksjoe@live.com>
2019-05-22 13:35:10 +0100

    Fix .gitignore to ignore .coveralls.yml

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,15 +1,15 @@
-const previewAdBtns = document.querySelectorAll(".car-image");
-const carPreview = document.getElementById("car-preview-overlay");
-const closeCarPreview = document.getElementById("close-car-preview");
+const previewAdBtns = document.querySelectorAll('.car-image');
+const carPreview = document.getElementById('car-preview-overlay');
+const closeCarPreview = document.getElementById('close-car-preview');
 
-previewAdBtns.forEach(btn => {
+previewAdBtns.forEach((btn) => {
   btn.onclick = () => {
-    carPreview.style.display = "block";
+    carPreview.style.display = 'block';
     toggleScroll();
   };
 });
 
 closeCarPreview.onclick = () => {
-  carPreview.style.display = "none";
+  carPreview.style.display = 'none';
   toggleScroll();
 };
