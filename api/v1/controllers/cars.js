85caf231286d51e38fbd01c8a4604e9ea8d64679
Chukwunonso Orjiakor <chuksjoe@live.com>
2019-05-25 16:58:06 +0100

    feature(create-car-ad-endpoint):create and test api endpoint for creating new car sale ad
    [Finishes #166196804]

---
+++ api/v1/controllers/cars.js
@@ -0,0 +1,48 @@
+import cars from '../models/cars';
+import users from '../models/users';
+
+export default {
+	// create a new car Ad and add it to car ads list
+	createNewCarAd(req, res) {
+		const owner = users.getAUserById(parseInt(req.body.owner_id, 10));
+		if (owner === null) {
+			return res.status(401).send({ status: 401, data: 'Unauthorized Access!' });
+		}
+		const { first_name, last_name, email } = owner;
+		const {
+			img_url, state, status, price, manufacturer,
+			model, body_type, fuel_type, description, mileage,
+			color, year, ac, arm_rest, fm_radio, dvd_player,
+			tinted_windows, air_bag, owner_id,
+		} = req.body;
+
+		const newCar = cars.createNewCar({
+			img_url,
+			name: `${state} ${manufacturer} ${model} - ${year}`,
+			owner_id: parseInt(owner_id, 10),
+			owner_name: `${first_name} ${last_name.charAt(0)}.`,
+			email,
+			created_on: Date(),
+			state,
+			status,
+			price: parseFloat(price, 10),
+			manufacturer,
+			model,
+			body_type,
+			fuel_type,
+			mileage: parseInt(mileage, 10),
+			color,
+			year: parseInt(year, 10),
+			description,
+			features: {
+				ac: ac === 'true',
+				arm_rest: arm_rest === 'true',
+				fm_radio: fm_radio === 'true',
+				dvd_player: dvd_player === 'true',
+				tinted_windows: tinted_windows === 'true',
+				air_bag: air_bag === 'true',
+			},
+		});
+		return res.status(201).send({ status: 201, data: newCar });
+	},
+};
