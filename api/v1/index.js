b723d4955527b71ceade80b8a3fe35083bb70e45
Chukwunonso Orjiakor <chuksjoe@live.com>
2019-05-30 14:57:26 +0100

    feature(serve-static-pages):serve static html pages with their resources
    change the links to the resources (css, js and images) by removing the 2 dots that preceeds the first /
    Eg: "/css/general.css" in place of "../css/general.css"
    [Finishes #166328672]

--- api/v1/index.js
+++ api/v1/index.js
@@ -2,21 +2,79 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import config from 'config';
 import logger from 'morgan';
+import path from 'path';
+import compression from 'compression';
 
 import router from './server';
 
 const debug = require('debug')('http');
 
 const app = express();
+const port = config.get('port');
+const prefix = '/api/v1';
 
 app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = config.get('port');
+// serve the api endpoints built in server.js
+app.use(prefix, router);
 
-app.use('/api/v1', router);
-app.get('*', (req, res) => res.status(200).send({ message: 'Welcome on Board: AutoMart API.' }));
+// serving the UI pages
+app.use(compression());
+
+const dirName = '../../UI/template';
+const resoursePath = path.join(__dirname, '../../UI');
+debug(`Resource Path: ${resoursePath}`);
+app.use(express.static(resoursePath));
+app.use('/css', express.static(path.join(__dirname, '../../UI/css')));
+app.use('/js', express.static(path.join(__dirname, '../../UI/js')));
+app.use('/images', express.static(path.join(__dirname, '../../UI/images')));
+
+app.get(`${prefix}`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/index.html'));
+});
+
+app.get(`${prefix}/index`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/index.html'));
+});
+
+app.get(`${prefix}/signup`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/signup.html'));
+});
+
+app.get(`${prefix}/signin`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/signin.html'));
+});
+
+app.get(`${prefix}/admin`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/admin.html'));
+});
+
+app.get(`${prefix}/marketplace`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/marketplace.html'));
+});
+
+app.get(`${prefix}/my-posted-ads`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/my-posted-ads.html'));
+});
+
+app.get(`${prefix}/post-new-ad`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/post-new-ad.html'));
+});
+
+app.get(`${prefix}/purchase-history`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/purchase-history.html'));
+});
+
+app.get(`${prefix}/sales-history`, (req, res) => {
+  res.sendFile(path.join(__dirname, dirName, '/sales-history.html'));
+});
+
+// default page is set to the index page
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, dirName, '/index.html'));
+});
 
 const listen = app.listen(port, () => {
 	debug(`AutoMart Server is running on port ${port} and in ${config.get('mode')} mode...`);
